e5de4477d395bae97e2e7309b9dcf6276b54a7be
jmelon66 <961255554@qq.com>
2024-05-14 22:19:31 +0800

    img color change&&map translate

--- src/components/mapView/index.js
+++ src/components/mapView/index.js
@@ -2,12 +2,15 @@
  * @Author: jmelon66 961255554@qq.com
  * @Date: 2024-04-04 10:18:12
  * @LastEditors: git config user.name && git config user.email
- * @LastEditTime: 2024-04-08 11:10:56
+ * @LastEditTime: 2024-04-10 13:43:03
  * @FilePath: \mapDemoJS\src\components\mapView\index.js
  * @Description: 主函数
  */
+import './index.less'
 import ImgM from './manager/imageM'
 class MapView {
+  dom = null
+  canvas = null
   // 地图文件
   mapImage = null
   constructor(element, options) {
@@ -22,7 +25,9 @@ class MapView {
   }
   // 初始化canvas
   async initCanvas(element) {
+    const pdom = document.createElement('div')
     const canvas = document.createElement('canvas')
+    pdom.className = '_map_view'
     canvas.id = 'map_view_entity'
     canvas.style.position = 'absolute'
     canvas.style.left = '0'
@@ -32,13 +37,67 @@ class MapView {
     canvas.width = element.clientWidth
     canvas.height = element.clientHeight
     this.mapImage = await ImgM.loadImageByNameAsync('map')
-    element.appendChild(canvas)
-    setTimeout(() => {
-      console.log(this.mapImage, canvas.width, canvas.height)
-      const ctx = canvas.getContext('2d')
-      ctx.globalCompositeOperation = 'destination-over'
-      ctx.drawImage(this.mapImage, 0, 0, canvas.width, canvas.height)
-    }, 500)
+    pdom.appendChild(canvas)
+    element.appendChild(pdom)
+    // 初始化
+    this.canvas = canvas
+    this.dom = element
+    this.draw(canvas)
+    this.initListener(pdom)
+  }
+
+  draw(canvas) {
+    const ctx = canvas.getContext('2d')
+    ctx.globalCompositeOperation = 'destination-over'
+    ctx.drawImage(this.mapImage, 0, 0, canvas.width, canvas.height)
+  }
+  // 鼠标状态 0 无，1 拖动
+  mouseType = 0
+  initListener(element) {
+    element.addEventListener('mousedown', this.mousedownListener.bind(this))
+    element.addEventListener('mousemove', this.mousehoverListener.bind(this))
+    window.addEventListener('mouseup', this.mouseupListener.bind(this))
+    element.addEventListener('mouseleave', this.mouseleaveListener.bind(this))
+  }
+  // 事件监听
+  mouseClientX = 0
+  mouseClientY = 0
+  translateX = 0
+  translateY = 0
+  mousedownListener(e) {
+    this.mouseType = 1
+    e.stopPropagation()
+    console.log('click')
+    this.mouseClientX = e.clientX
+    this.mouseClientY = e.clientY
+  }
+  animationIndex = 0
+  mousehoverListener(e) {
+    if (this.mouseType !== 1) return
+    this.translateX += e.clientX - this.mouseClientX
+    this.translateY += e.clientY - this.mouseClientY
+    this.mouseClientX = e.clientX
+    this.mouseClientY = e.clientY
+    if (!this.animationIndex) {
+      this.animationIndex = requestAnimationFrame(this.dragCanvas.bind(this))
+    }
+  }
+  mouseupListener(e) {
+    console.log('up')
+    this.mouseType = 0
+  }
+  mouseleaveListener(e) {
+    this.mouseType = 0
+  }
+  // css拖动
+  zoom = 1
+  dragCanvas() {
+    console.log('change')
+    cancelAnimationFrame(this.animationIndex)
+    this.animationIndex = null
+    const { canvas } = this
+    // canvas.style.translateX =
+    canvas.style.transform = `translateX(${this.translateX}px) translateY(${this.translateY}px)`
   }
 }
 export default MapView
--- src/components/mapView/manager/imageM.js
+++ src/components/mapView/manager/imageM.js
@@ -1,5 +1,66 @@
+/*
+ * @Date: 2024-04-04 10:20:06
+ * @LastEditors: git config user.name && git config user.email
+ * @LastEditTime: 2024-04-29 15:34:55
+ * @FilePath: \mapDemoJS\src\components\mapView\manager\imageM.js
+ * @Description:
+ * @Author: ms-tlzksaoastkh
+ */
 const imageO = {
   map: new URL('../assets/map.png', import.meta.url).href
+}
+function RGBtoHSI([r, g, b]) {
+  const R = r / 255
+  const G = g / 255
+  const B = b / 255
+  const theTa =
+    R === 0 && G === 0 && B === 0
+      ? 0
+      : (Math.acos((R - G + R - B) / 2 / Math.sqrt(Math.pow(R - G, 2) + (R - B) * (G - B))) * 180) /
+        Math.PI
+  const H = B <= G ? theTa : 360 - theTa
+  const sA = R + G + B
+  const sB = 1 - Math.min(...[R, G, B]) * 3
+  const S = sA === 0 ? 0 : sB / sA
+  const I = (R + G + B) / 3
+  return [H, S, I]
+}
+function HSItoRGB([H, S, I]) {
+  if (H >= 120 && H <= 240) {
+    H -= 120
+  } else if (H >= 240 && H <= 360) {
+    H -= 240
+  }
+  const HA = (H * Math.PI) / 180
+  const ca = Math.cos(Math.PI / 3 - HA)
+  const a1 = I * (1 - S)
+  const a2 = ca === 0 ? 0 : I * (1 + (S * Math.cos(HA)) / ca)
+  const a3 = 3 * I - (a1 + a2)
+  if (H >= 120 && H <= 240) {
+    return [a1, a2, a3]
+  } else if (H >= 240 && H <= 360) {
+    return [a3, a1, a2]
+  } else {
+    return [a2, a3, a1]
+  }
+}
+function HucChange(imageData, rgbC) {
+  const hsiC = RGBtoHSI(rgbC)
+  const resA = []
+  for (let i = 0; i < imageData.length; i += 4) {
+    const rgb = []
+    rgb.push(imageData[i])
+    rgb.push(imageData[i + 1])
+    rgb.push(imageData[i + 2])
+    rgb.push(imageData[i + 3])
+    const hsi = RGBtoHSI(rgb)
+    hsi[0] += hsiC[0]
+    hsi[1] += hsiC[1]
+    hsi[2] += hsiC[2]
+    resA.push(...HSItoRGB(hsi).map((n) => Math.floor(n * 255)))
+    resA.push(imageData[i + 3])
+  }
+  return resA
 }
 const ImgM = {
   imageO,
@@ -8,8 +69,33 @@ const ImgM = {
     return new Promise((resolve) => {
       image.src = imageO[name]
       image.onload = function () {
-        resolve(image)
+        ImgM.fixedImageColor(image).then((res) => {
+          resolve(res)
+        })
       }
+    })
+  },
+  fixedImageColor(image) {
+    return new Promise((resolve) => {
+      const canvas = document.createElement('canvas')
+      canvas.style.width = image.width
+      canvas.style.height = image.height
+      canvas.width = image.width
+      canvas.height = image.height
+      const ctx = canvas.getContext('2d')
+      console.log(canvas)
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      const imageData2 = HucChange(imageData.data, [255, 0, 0])
+
+      console.log(imageData, imageData2)
+      const imageD = new ImageData(new Uint8ClampedArray(imageData2), canvas.width, canvas.height)
+      ctx.putImageData(imageD, 0, 0)
+      const img = new Image()
+      img.onload = () => {
+        resolve(img)
+      }
+      img.src = canvas.toDataURL()
     })
   }
 }
